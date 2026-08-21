7942d61cf0372a6a0a72b792cdd8b5019daf5d81
Trung <trung2007hs@gmail.com>
2026-08-21 14:41:16 +0700

    Auto update code

--- app/core/security.py
+++ app/core/security.py
@@ -7,4 +7,4 @@ def hash_password(password: str) -> str:
 def verify_password(password: str, hash_password: str) -> str:
     return bcrypt.checkpw(password.encode(), hash_password.encode())
 
-def create_access_token()
+def create_access_token(data: dict) -> dict:
