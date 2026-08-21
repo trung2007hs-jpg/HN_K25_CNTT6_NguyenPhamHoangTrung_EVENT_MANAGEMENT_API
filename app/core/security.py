a7b3440a47958f100b99bec11bc233543fa0e900
Trung <trung2007hs@gmail.com>
2026-08-21 14:47:55 +0700

    Auto update code

--- app/core/security.py
+++ app/core/security.py
@@ -1,10 +1,16 @@
+from datetime import datetime, timezone, timedelta
 import bcrypt
 from core.config import settings
+import jwt
 
 def hash_password(password: str) -> str:
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
 
-def verify_password(password: str, hash_password: str) -> str:
+def verify_password(password: str, hash_password: str) -> bool:
     return bcrypt.checkpw(password.encode(), hash_password.encode())
 
-def create_access_token(data: dict) -> dict:
+def create_access_token(data: dict):
+  to_encode = data.copy()
+  expire = datetime.now(timezone.utc) + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+  to_encode.update({"exp": expire})
+  return jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
